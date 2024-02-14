4386397be1f835050e6e91d1b8f2c4d6dd5c8547
Carl Centola <carl.centola@gmail.com>
2024-02-14 15:21:05 -0700

    crawl page recursive

--- crawl.js
+++ crawl.js
@@ -1,6 +1,20 @@
 const { JSDOM } = require('jsdom');
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+  const baseURLObj = new URL(baseURL);
+  const currentURLObj = new URL(currentURL);
+  if (baseURLObj.hostname !== currentURLObj.hostname) {
+    return pages;
+  }
+
+  const normalizedCurrentURL = normalizeURL(currentURL);
+  if (pages[normalizedCurrentURL] > 0) {
+    pages[normalizedCurrentURL]++;
+    return pages;
+  }
+
+  pages[normalizedCurrentURL] = 1;
+
   console.log(`actively crawling ${currentURL}...`);
 
   try {
@@ -9,19 +23,26 @@ async function crawlPage(currentURL) {
       console.log(
         `error in fetch with status code: ${response.status} on page: ${currentURL} `
       );
-      return;
+      return pages;
     }
     const contentType = response.headers.get('content-type');
     if (!contentType.includes('text/html')) {
       console.log(
         `non html response, content type: ${response} on page: ${currentURL} `
       );
-      return;
+      return pages;
     }
-    console.log(await response.text());
+    const htmlBody = await response.text();
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages);
+    }
   } catch (err) {
     console.log(`error in fetch: ${err.message}, on page ${currentURL}`);
   }
+
+  return pages;
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
