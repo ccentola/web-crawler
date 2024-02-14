7acec61ee502917812fb3249a27268d6e32bce12
Carl Centola <carl.centola@gmail.com>
2024-02-14 15:05:06 -0700

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,29 @@
 const { JSDOM } = require('jsdom');
 
+async function crawlPage(currentURL) {
+  console.log(`actively crawling ${currentURL}...`);
+
+  try {
+    const response = await fetch(currentURL);
+    if (response.status > 399) {
+      console.log(
+        `error in fetch with status code: ${response.status} on page: ${currentURL} `
+      );
+      return;
+    }
+    const contentType = response.headers.get('content-type');
+    if (!contentType.includes('text/html')) {
+      console.log(
+        `non html response, content type: ${response} on page: ${currentURL} `
+      );
+      return;
+    }
+    console.log(await response.text());
+  } catch (err) {
+    console.log(`error in fetch: ${err.message}, on page ${currentURL}`);
+  }
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
   const urls = [];
   const dom = new JSDOM(htmlBody);
@@ -35,4 +59,4 @@ function normalizeURL(urlString) {
   return hostPath;
 }
 
-module.exports = { normalizeURL, getURLsFromHTML };
+module.exports = { normalizeURL, getURLsFromHTML, crawlPage };
