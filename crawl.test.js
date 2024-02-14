4f29c382abece7361067b8c9b573e4972557d26b
Carl Centola <carl.centola@gmail.com>
2024-02-14 12:05:46 -0700

    normalizeURL and tests

---
+++ crawl.test.js
@@ -0,0 +1,30 @@
+const { normalizeURL } = require('./crawl.js');
+const { test, expect } = require('@jest/globals');
+
+test('normailzeURL strip protocol', () => {
+  const input = 'https://blog.boot.dev/path';
+  const actual = normalizeURL(input);
+  const expected = 'blog.boot.dev/path';
+  expect(actual).toEqual(expected);
+});
+
+test('normailzeURL strip trailing slash', () => {
+  const input = 'https://blog.boot.dev/path/';
+  const actual = normalizeURL(input);
+  const expected = 'blog.boot.dev/path';
+  expect(actual).toEqual(expected);
+});
+
+test('normailzeURL capitals', () => {
+  const input = 'https://BLOG.boot.dev/path/';
+  const actual = normalizeURL(input);
+  const expected = 'blog.boot.dev/path';
+  expect(actual).toEqual(expected);
+});
+
+test('normailzeURL strip http', () => {
+  const input = 'http://blog.boot.dev/path/';
+  const actual = normalizeURL(input);
+  const expected = 'blog.boot.dev/path';
+  expect(actual).toEqual(expected);
+});
